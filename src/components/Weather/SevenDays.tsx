40c8f3ec46b71d71127bd611ef55b8264efe376f
toto42 <toto42@yopmail.com>
2020-09-29 22:36:02 +0300

    css fixed, but don't know about bootstrap, should install if stil doesn't work

--- src/components/Weather/SevenDays.tsx
+++ src/components/Weather/SevenDays.tsx
@@ -29,23 +29,26 @@ export default function SevenDays(props: any) {
         }
       </div >
       <h1>7 days </h1>
-      {
-        state.data && (
-          state.data.daily.map((day: any, i: number) => <div key={i} className="d-inline-flex ml-1 mr-1">
-            <div style={{ backgroundColor: "turquoise", width: '110px', borderRadius: "8px" }}>
-              <div className="city-temp">
-                {Math.round(day.temp.day)}<sup>°C</sup>
+      <div className="ml-1 mr-1" style={{ display: "flex", flexWrap: "nowrap", overflowX: "auto", justifyContent: "center" }}>
+
+        {
+          state.data && (
+            state.data.daily.map((day: any, i: number) => <div key={i} className="d-inline-flex ml-1 mr-1">
+              <div style={{ backgroundColor: "turquoise", width: '110px', borderRadius: "8px" }}>
+                <div className="city-temp">
+                  {Math.round(day.temp.day)}<sup>°C</sup>
+                </div>
+                <div className="info">
+                  <img className="city-icon" src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt={day.weather[0].description} />
+                  <p style={{ color: "whitesmoke" }}>{day.weather[0].description}</p>
+                </div>
+                <p style={{ fontSize: "1rem", }}>{moment().add(i, 'days').format("dddd, MMMM Do")}</p>
               </div>
-              <div className="info">
-                <img className="city-icon" src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt={day.weather[0].description} />
-                <p style={{ color: "whitesmoke" }}>{day.weather[0].description}</p>
-              </div>
-              <p style={{ fontSize: "1rem", }}>{moment().add(i, 'days').format("dddd, MMMM Do")}</p>
             </div>
-          </div>
+            )
           )
-        )
-      }
+        }
+      </div >
     </div >
   );
 }
