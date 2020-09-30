91e15805e79033c2b59bcbde5fed30ec59d8b2eb
toto42 <toto42@yopmail.com>
2020-09-30 20:16:04 +0300

    collapse add

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import { convert } from './components/utils/Currency';
 // import Button from './components/button/Button';
 import Counter from './components/Counter/Counter';
 import Forecast from './components/Weather/Forecast';
+// import bulmaCollapsible from '@creativebulma/bulma-collapsible';
+
 
 function App() {
   const [conversion, setConversion] = React.useState<string>("");
   const [counter, setCounter] = React.useState<number>(0);
+  const [collapse, setCollapse] = React.useState<Boolean>(false);
 
   React.useEffect(() => {
     const fetchData = async () => {
@@ -27,26 +30,36 @@ function App() {
         background: `linear-gradient(to right, rgb(255, 167, 35), rgb(25, 97, 235))`,
         minHeight: "100vh"
       }}>
+
         <Forecast />
-        <Counter />
-        <h1>conversion rate : $ USD - € EUR</h1>
-        <h3>{conversion ? conversion : ''} : rate $ / €</h3>
-        <h2>CI-CD</h2>
-        {/* <Button label="click me please"></Button> 
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p> */}
-        <div className="d-inline-flex p-2">
-          <button id="increment-btn" className="button mr-2" style={{ border: "1px solid yellow", borderRadius: "8px", padding: "3px 3px" }}
-            onClick={() => setCounter(counter + 1)}>+ Increment</button>
-          <button id="decrement-btn" className="button" style={{ border: "1px solid yellow", borderRadius: "8px", padding: "3px 3px" }}
-            onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>- Decrement</button>
-        </div>
-        <p>Conversion {(counter / parseFloat(conversion)).toFixed(2)} $</p>
-        <div className="d-inline-flex p-2">
-          <span id="counter-value">{counter}</span>
-          <span> €</span>
-        </div>
+
+        <button type="button" className="collapsible mt-5 has-text-centered" onClick={() => setCollapse(!collapse)}>
+          Open Conversion $/€ debug<i className="fa fa-angle-down"></i>
+        </button>
+        {collapse && (<div className="content">
+          <div className="card">
+            <Counter />
+            <h1>conversion rate : $ USD - € EUR</h1>
+            <h3>{conversion ? conversion : ''} : rate $ / €</h3>
+            <h2>CI-CD</h2>
+            {/* <Button label="click me please"></Button> 
+                <p>
+                  Edit <code>src/App.tsx</code> and save to reload.
+                </p> */}
+            <div className="d-inline-flex p-2">
+              <button id="increment-btn" className="button mr-2" style={{ border: "1px solid yellow", borderRadius: "8px", padding: "3px 3px" }}
+                onClick={() => setCounter(counter + 1)}>+ Increment</button>
+              <button id="decrement-btn" className="button" style={{ border: "1px solid yellow", borderRadius: "8px", padding: "3px 3px" }}
+                onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>- Decrement</button>
+            </div>
+            <p>Conversion {(counter / parseFloat(conversion)).toFixed(2)} $</p>
+            <div className="d-inline-flex p-2">
+              <span id="counter-value">{counter}</span>
+              <span> €</span>
+            </div>
+          </div>
+        </div>)}
+
       </div>
     </div>
   );
