5abfc42a1c723990d0c295d3a8fc05b563639b7b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-01 12:24:37 +0000

    Deploy rim31/CICD-Jest to github.com/rim31/CICD-Jest.git:gh-pages

---
+++ precache-manifest.47c408af4d132926113763431e4b4cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bddf49b044b1225fc803e0a37b4b7b",
+    "url": "/CICD-Jest/index.html"
+  },
+  {
+    "revision": "163c9fec7da059adb243",
+    "url": "/CICD-Jest/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "410d9366fff0fb5fc2f2",
+    "url": "/CICD-Jest/static/css/main.cf1884ff.chunk.css"
+  },
+  {
+    "revision": "163c9fec7da059adb243",
+    "url": "/CICD-Jest/static/js/2.fb4fb1fc.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/CICD-Jest/static/js/2.fb4fb1fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410d9366fff0fb5fc2f2",
+    "url": "/CICD-Jest/static/js/main.e13187b0.chunk.js"
+  },
+  {
+    "revision": "0c70c558be2ed1fceee8",
+    "url": "/CICD-Jest/static/js/runtime-main.6f0af2b2.js"
+  }
+]);
